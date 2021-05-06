55acf98ca3af201bf7a5069365462cce04471aaa
setareh askari s288485 <s288485@studenti.polito.it>
2021-05-06 16:25:23 +0200

    implement some models

--- src/main/java/it/polito/ezshop/data/model/BalanceOperationClass.java
+++ src/main/java/it/polito/ezshop/data/model/BalanceOperationClass.java
@@ -1,5 +1,7 @@
 package it.polito.ezshop.data.model;
 
+import it.polito.ezshop.data.BalanceOperation;
+
 public class BalanceOperationClass implements BalanceOperation {
 	
 	private Integer balanceId ;
--- src/main/java/it/polito/ezshop/data/model/CustomerClass.java
+++ src/main/java/it/polito/ezshop/data/model/CustomerClass.java
@@ -1,4 +1,50 @@
 package it.polito.ezshop.data.model;
 
-public class CustomerClass {
+import it.polito.ezshop.data.Customer;
+
+public class CustomerClass implements Customer {
+
+    private Integer id;
+    private String customerName;
+    private String customerCard;
+
+    @Override
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    @Override
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    @Override
+    public String getCustomerCard() {
+        return this.customerCard;
+    }
+
+    @Override
+    public void setCustomerCard(String customerCard) {
+        this.customerCard = customerCard;
+    }
+
+    @Override
+    public Integer getId() {
+        return this.id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getPoints() {
+       // get it from customer repository
+    }
+
+    @Override
+    public void setPoints(Integer points) {
+         // set the point in repository and ....
+    }
 }
