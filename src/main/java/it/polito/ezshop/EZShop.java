87a7ff3ed127961a01bb7bbc3a34cb531b11a66b
Eugenio Ressa <s281642@studenti.polito.it>
2021-05-17 16:56:45 +0200

    getCreditAndDebit error caused by rs.getDate() solved

--- src/main/java/it/polito/ezshop/EZShop.java
+++ src/main/java/it/polito/ezshop/EZShop.java
@@ -1,5 +1,7 @@
 package it.polito.ezshop;
 
+import java.time.LocalDate;
+
 import it.polito.ezshop.view.EZShopGUI;
 
 
@@ -11,7 +13,10 @@ public class EZShop {
         try {                              
            it.polito.ezshop.data.EZShop ezShop = new it.polito.ezshop.data.EZShop();
            EZShopGUI gui = new EZShopGUI(ezShop);
-           
+           //ezShop.createUser("eugenio", "eugenio", "Administrator");
+          // ezShop.login("eugenio", "eugenio");
+          // ezShop.recordBalanceUpdate(20.0);
+           //System.out.println(ezShop.getCreditsAndDebits(LocalDate.of(2021, 5, 17), LocalDate.of(2021, 5, 17)));
         }catch(Exception e){           
      	   e.printStackTrace();
         }
