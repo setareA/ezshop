7979150c1a1b9bc7d5ecff03ba293d70cf69057e
setareh askari <s288485@studenti.polito.it>
2021-05-18 23:12:16 +0200

    CustomerClassTest done

--- src/test/java/it/polito/ezshop/TestEZShop.java
+++ src/test/java/it/polito/ezshop/TestEZShop.java
@@ -1,12 +1,14 @@
 package it.polito.ezshop;
 
 
+import it.polito.ezshop.data.model.CustomerClass;
+import it.polito.ezshop.data.model.CustomerClassTest;
 import it.polito.ezshop.data.model.UserClassTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({UserClassTest.class})
+@Suite.SuiteClasses({UserClassTest.class, CustomerClassTest.class})
     public class TestEZShop {
 
 
