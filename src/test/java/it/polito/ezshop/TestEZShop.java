88fbf719a25ff2e69c64db468b98be69b6db4ab1
setareh askari <s288485@studenti.polito.it>
2021-05-18 14:38:13 +0200

    add test suite

--- src/test/java/it/polito/ezshop/TestEZShop.java
+++ src/test/java/it/polito/ezshop/TestEZShop.java
@@ -1,10 +1,22 @@
 package it.polito.ezshop;
 
 
+import it.polito.ezshop.data.model.OrderClassTest;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
 import static org.junit.Assert.*;
 
 
 public class TestEZShop {
+    public static void main(String[] args) {
+        suite();
+    }
 
-	 
+    public static Test suite() {
+        TestSuite suite = new TestSuite("EZShop Test");
+        // suite.addTest(new TestSuite(OrderClassTest.class));
+        return suite;
+    }
+
 }
